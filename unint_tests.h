034c3bd8b55d4ebda43e0491835eea70a9d35521
Дмитрий Туминов <d.tuminov1@outlook.com>
2021-10-15 16:38:08 +0300

    first try to write unot-tests)

---
+++ unint_tests.h
@@ -0,0 +1,6 @@
+#include "quadratic_equation.h"
+ typedef enum {OK, ligma} Errors;
+
+void run_all_tests();
+Errors test_solve_linear_equation();
+Errors test_solve_quadratic_equation();
--- unit_tests.c
+++ unit_tests.c
@@ -1,24 +1,74 @@
+#include "unint_tests.h"
 #include <stdio.h>
-#include <math.h>
 #include <assert.h>
-#include "quadratic_equation.c"
-// test of the linear equation solver unction
-void testSolveLineProblem(){
-    double x1 = 0, x2 = 0;
-    assert(solveLineProblem(0,0,&x1,&x2) == Inappropriate_b);
-    x1 = 0, x2 = 0;
-    assert(solveLineProblem(0,5,&x1,&x2) == Linear_equation);
-    x1 = -1, x2 = -1;
-    assert(solveLineProblem(5,5,&x1,&x2) == Linear_equation);
+
+void run_all_tests() { 
+    assert(OK == test_solve_linear_equation());
+    assert(OK == test_solve_quadratic_equation());
+} 
+Errors test_solve_linear_equation(){
+    double test_value1 = 5;
+    double test_value2 = 10;
+    double *test_pointer1 = NULL;
+    double *test_pointer2 = NULL;
+    exceptions returned_value = Linear_one_solution;
+
+    if (returned_value != solveLineProblem(test_value1, test_value2,test_pointer1,test_pointer2)) {
+        printf("F*ck this sh*t I'm out\n");
+        return ligma;
+    }
+    test_value1 = 0;
+    test_value2 = 10;
+    *test_pointer1 = NULL;
+    *test_pointer2 = NULL;
+    returned_value = Linear_no_solution;
+    if (returned_value != solveLineProblem(test_value1, test_value2,test_pointer1,test_pointer2)) {
+        printf("F*ck this sh*t I'm out\n");
+        return ligma;
+    }
+    test_value1 = 10;
+    test_value2 = 0;
+    *test_pointer1 = NULL;
+    *test_pointer2 = NULL;
+    returned_value = Linear_infinity_solution;
+    if (returned_value != solveLineProblem(test_value1, test_value2,test_pointer1,test_pointer2)) {
+        printf("F*ck this sh*t I'm out\n");
+        return ligma;
+    }
+
+    return OK;
 }
-// test of print unction 
-/*void testPrint_ans(){
-    assert(print_ans(1,2, Two_solutions) == "x1 = 1\nx2 = 2");
-    assert(print_ans(1,1, One_solution) == "I solved the quadratic equation, where x1=x2\nx=1");
-    assert(print_ans(1,1, Linear_equation) == "I solved the linear equation\nx=1");
-    assert(print_ans(1,1, Inappropriate_c) == "coefficient c = 0, cannot be divided by zero\n";
-    assert(print_ans(0,0, No_solutions) == "D < 0 ; x belongs to the empty set\n")
-}*/
-// test o quadratic equation solver function
-void testSolveSquareProblem(){
+
+Errors test_solve_quadratic_equation() {
+    double test_lvalue1 = 1;
+    double test_lvalue2 = 17;
+    double test_value2 = -18;
+    double *test_pointer1 = NULL;
+    double *test_pointer2 = NULL; 
+    exceptions returned_value = Two_solutions;
+    if (returned_value != solveSquareProblem(test_lvalue1, test_lvalue2, test_lvalue3,test_pointer1, test_pointer2)) {
+        printf("F*ck this sh*t I'm out\n");
+        return ligma;
+    }
+    test_lvalue1 = 1;
+    test_lvalue2 = 4;
+    test_lvalue2 = 4;
+    returned_value = One_solution;
+    *test_pointer1 = NULL;
+    *test_pointer2 = NULL;
+    if (returned_value != solveSquareProblem(test_lvalue1, test_lvalue2, test_lvalue3,test_pointer1, test_pointer2)) {
+        printf("F*ck this sh*t I'm out\n");
+        return ligma;
+    }
+    test_lvalue1 = 1;
+    test_lvalue2 = 2;
+    test_lvalue2 = 3;
+    returned_value = No_solutions;
+    *test_pointer1 = NULL;
+    *test_pointer2 = NULL;
+    if (returned_value != solveSquareProblem(test_lvalue1, test_lvalue2, test_lvalue3,test_pointer1, test_pointer2)) {
+        printf("F*ck this sh*t I'm out\n");
+        return ligma;
+    }
+    return OK;
 }
